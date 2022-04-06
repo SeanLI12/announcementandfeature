45c8123a920df50f08f26a6b1714b51d173f5ef1
Sean <larcenciel12@gmail.com>
2022-04-06 18:22:46 +0800

    first comment

---
+++ js/trans.js
@@ -0,0 +1,460 @@
+// Data for translation
+const translationData = [
+  // My Events
+  {
+    trans_id: "mes_banner_title",
+    en: "My Events",
+    zh: "我的赛事",
+    vi: "Trận Đấu Của Tôi",
+    id: "Favorit Saya",
+    th: "การแข่งขันของฉัน",
+    ko: "경기 즐겨찾기",
+    pt: "Meus Eventos",
+    km: "ការប្រកួតរបស់ខ្ញុំ",
+    ja: "マイイベント",
+  },
+  {
+    trans_id: "mes_banner_content",
+    en: [
+      "Selecting the star icon, for any event, will add it to the My Events section. Here, you can group and easily access all the events you are interested in.",
+      
+    ],
+    zh: [
+      "点击任意赛事的星型图标即可添加至“我的赛事”栏目。在这里，您可以轻松查看到所选赛事的相关盘口。",
+    ],
+    vi: [
+      "Giờ đây bằng cách nhấn vào biểu tượng ngôi sao tại bất kỳ trận đấu, Quý Khách có thể lưu & theo dõi các trận đấu yêu thích dễ dàng hơn tại Trận Đấu Của Tôi.",
+    ],
+    id: [
+      "Tandai ikon bintang, untuk semua pertandingan, akan menambahkannya ke bagian Favorit. Di sini, Anda dapat dengan mudah mengelompokkan dan mengakses ke semua pertandingan yang diminati Anda.",
+    ],
+    th: [
+      "กดติดดาวให้กับการแข่งขันที่คุณต้องการติดตามผล ระบบจะแสดงแมทช์ที่คุณติดดาวรวมไว้ในการแข่งขันของฉัน เพื่อให้ง่ายต่อการเข้าถึง",
+    ],
+    ko: [
+      "별 모양 아이콘을 눌러 다른 경기를 '경기 즐겨찾기'에 추가할 수 있습니다. 이곳에서 관심 있는 모든 경기를 쉽게 확인하고 접근할 수 있습니다.",
+    ],
+    pt: [
+      "Selecionando o ícone da estrela, para qualquer evento, irá adicioná-lo à seção Meus Eventos. Aqui, você pode agrupar e acessar facilmente todos os eventos que você está interessado.",
+    ],
+    km: [
+      "សូមជ្រើសរើសរូបផ្កាយសម្រាប់ព្រឹត្តិការណ៍ណាមួយ ហើយវានឹងបន្ថែមទៅផ្នែកព្រឹត្តិការណ៍របស់ខ្ញុំ។ទីនេះ អ្នកអាចដាក់ជាក្រុម និងចូលដំណើរការព្រឹត្តិការណ៍ទាំងអស់ដែលអ្នកបានចាប់អារម្មណ៍បានយ៉ាងងាយស្រួល។",
+    ],
+    ja: [
+      "イベントのスターアイコンを選択すると、[マイイベント]セクションに追加できます。ここでは、関心のある全てのイベントをグループ化して簡単にアクセスできます。",
+    ]
+  },
+  // Favorite Sports
+  {
+    trans_id: "fss_banner_title",
+    en: "Favorite Sports",
+    zh: "最爱项目",
+    vi: "Môn Thể Thao Yêu Thích",
+    id: "Olahraga Favorit",
+    th: "กีฬาโปรด",
+    ko: "선호하는 스포츠",
+    pt: "Esportes Favoritos",
+    km: "កីឡាដែលចូលចិត្ត",
+    ja: "お気に入りスポーツ",
+  },
+  {
+    trans_id: "fss_banner_content",
+    en: ["Selecting the star icon, beside any sport in the Left Menu, will prioritize the position of this sport within the In-Play and Prestart odds pages."],
+    zh: ['點選左上菜單圖標後再點選體育名稱旁的星型图标可把滾球和早盤頁的體育的順序往上移。'],
+    vi: ["Mở 'Danh sách' ở phía trên bên trái và nhấn vào biểu tượng ngôi sao tại bất kỳ môn thể thao để ưu tiên hiển thị thông tin tại trang cược."],
+    id: ["Tandai ikon bintang, yang terletak di sebelah olahraga apa pun pada Menu bagian Kiri, akan memprioritaskan posisi olahraga ini dalam halaman odds In-Play dan Awal."],
+    th: ['กดติดดาวให้กับกีฬาโปรดของคุณ บริเวณเมนูกีฬาด้านซ้าย ระบบจะแสดงเดิมพันกีฬานี้ไว้เป็นลำดับแรก เพื่อให้ง่ายต่อการเข้าถึง'],
+    ko: ['경기 시작 전 혹은 인플레이(실시간) 배당률 페이지에서 별 모양 아이콘을 눌러 왼쪽 메뉴에 명시된 스포츠 목록을 내가 원하는 순서대로 변경할 수 있습니다.'],
+    pt: ['Selecionando o ícone da estrela, ao lado de qualquer esporte no menu esquerdo, priorizará a posição desse esporte nas páginas de cotações em Jogo Ao-Vivo e Pré-partida.'],
+    km: ["សូមជ្រើសរើសរូបផ្កាយ ក្រៅពីកីឡាណាមួយនៅក្នុងមឺនុយខាងឆ្វេង វានឹងផ្តល់អាទិភាពដល់ទីតាំងនៃកីឡានេះនៅក្នុងទំព័រហាងឆេងកីឡាកំពុងលេង និងកីឡានឹងចាប់ផ្តើមឆាប់ៗ។"],
+    ja: ["左メニューのスポーツの横にあるスターアイコンを選択すると、インプレイオッズページとプレスタートオッズページ内でこのスポーツ表示が優先されます。"],
+  },
+  {
+    trans_id: "new_sportsbook",
+    en: "New Sportsbook",
+    zh: "新体育界面",
+    vi: "Giao Diện Thể Thao Mới",
+    id: "Tampilan Baru Olahraga",
+    th: "หน้ากีฬาโฉมใหม่",
+    ko: "새로운 스포츠 페이지",
+    pt: "Novo Sportsbook",
+    km: "ទំព័រកីឡាថ្មី",
+    ja: "NEWスポーツブック",
+  },
+  {
+    trans_id: "introduction",
+    en:
+      "Welcome to our new Sportsbook. We are still enhancing and fine-tuning but we wanted to give you an early chance to experience the new features. We hope you like it!",
+    zh:
+      "欢迎您抢先体验全新功能的体育游戏界面，未来我们仍会持续优化，希望您会喜欢!",
+    vi:
+      "Chào mừng đến với giao diện mới của Trang Thể thao. Chúng tôi vẫn không năng nâng cấp và hoàn thiện tốt hơn, nhưng chúng tôi muốn mang đến cho Quý Khách cơ hội sớm để trải nghiệm các tính năng mới này.",
+    id:
+      "Selamat datang di Tampilan Baru Olahraga. Kami masih berada dalam tahap peningkatan dan penyempurnaan, tetapi kami ingin memberi Anda kesempatan awal untuk merasakan fitur baru. Semoga Anda menyukainya!",
+    th:
+      "ยินดีต้อนรับสู่หน้ากีฬาโฉมใหม่ เรามีการปรับปรุงและพัฒนาอยู่เสมอ และโอกาสนี้เราอยากให้คุณได้สัมผัสกับหน้ากีฬามือถือโฉมใหม่ก่อนใคร ลองเลย!",
+    ko:
+      "새로운 스포츠 페이지에 오신 것을 환영합니다. 당사의 스포츠 페이지는 더 나은 서비스 제공을 위해 새 단장 중에 있습니다. 다양한 경험을 위해, 새로운 스포츠 페이지 프리뷰를 제공하오니 즐거운 경험 되시길 바랍니다!",
+    pt:
+      "Bem Vindo ao nosso novo Sportsbook. Ainda estamos aprimorando e ajustando, mas queríamos dar a você a oportunidade antecipada de experimentar os novos recursos. Esperamos que você goste!",
+    km:
+      "សូមស្វាគមន៍មកកាន់ទំព័រកីឡាថ្មីរបស់យើងខ្ញុំ។ យើងនៅតែស្ថិតក្នុងដំណាក់កាលកែលម្អ និងអភិវឌ្ឍឥតឈប់ឈរ ប៉ុន្តែយើងចង់ផ្តល់ឱ្យអ្នកនូវឱកាសដំបូងដើម្បីទទួលបានបទពិសោធន៍ថ្មីៗ។ យើងសង្ឃឹមថាអ្នកចូលចិត្តវា!",
+    ja:
+      "NEWスポーツブックへようこそ。 現在、機能強化調整の最中ではありますが、新機能をいち早くご体験いただきたいと思っております。ぜひ、お試しください！",
+  },
+  // FEATURE 1
+  {
+    trans_id: "feature_1_header",
+    en: "Introducing our new design",
+    zh: "了解我们全新的设计",
+    vi: "Giới Thiệu Giao Diện Mới",
+    id: "Pengenalan tampilan baru kami",
+    th: "ขอแนะนำหน้ากีฬาโฉมใหม่ของเรา",
+    ko: "새로운 스포츠 페이지를 소개합니다",
+    pt: "Apresentamos nosso novo design",
+    km: "ការណែនាំពីការរចនាថ្មីរបស់យើង។",
+    ja: "NEWデザインのご紹介",
+  },
+  {
+    trans_id: "feature_1_content",
+    en: [
+      "A modern look, with new features.",
+      "You can learn about some of these new features below.",
+      "We hope you like the new design and thank you for your continued support.",
+    ],
+    zh: [
+      "全新功能以及崭新界面已上线，您可以在下方了解详情。",
+      "您可以在下方了解新的功能",
+      "希望您会喜欢这个全新的设计，感谢您的支持。",
+    ],
+    vi: [
+      "Thuận tiện hơn, tích hợp nhiều tính năng.",
+      "Quý Khách có thể tìm hiểu về một số điểm nổi bật về các tính năng bên dưới.",
+      "Hy vọng Quý Khách thích giao diện mới này. Cám ơn Quý.",
+    ],
+    id: [
+      "Tampilan modern, dengan fitur baru.",
+      "Anda bisa pelajari tentang beberapa hal mengenai fitur-fitur di bawah ini.",
+      "Semoga Anda menyukai tampilan baru kami dan terima kasih atas dukungan Anda yang tiada henti.",
+    ],
+    th: [
+      "การดีไซน์ที่ทันสมัย พร้อมฟีเจอร์ใหม่มากมาย",
+      "พร้อมฟีเจอร์ต่าง ๆ มากมาย คุณสามารถดูข้อมูลได้ตามรายละเอียดด้านล่าง ",
+      "เราหวังว่าคุณจะชอบหน้ากีฬาโฉมใหม่ของเรา และขอขอบคุณที่ให้การสนับสนุน ด้วยดีเสมอมา",
+    ],
+    ko: [
+      "새로운 항목과 모던한 페이지.",
+      "아래의 설명을 통해 각 항목을 확인하실 수 있습니다. ",
+      "새로운 스포츠 페이지를 통해 즐거운 경험 하시기 바라며, 계속되는 고객님들의 성원에 감사드립니다.",
+    ],
+    pt: [
+      "Um look moderno, com novos recursos.",
+      "Você poderá aprender mais sobre estes novos recursos abaixo.",
+      "Esperamos que você goste do nosso novo design e obrigado pelo seu apoio.",
+    ],
+    km: [
+      "ការរចនាបែបទំនើប ជាមួយនឹងលក្ខណៈពិសេសថ្មីៗ។",
+      "អ្នកអាចស្វែងរកព័ត៌មានស្តីពីលក្ខណៈពិសេសថ្មីៗទាំងនេះនៅខាងក្រោម។",
+      "យើងសង្ឃឹមថាអ្នកចូលចិត្តការរចនាថ្មីនេះ ហើយសូមអរគុណខ្លាំងណាស់ចំពោះការបន្តនូវការគាំទ្ររបស់អ្នក។",
+    ],
+    ja: [
+      "新機能を備えた近代デザイン。",
+      "一部の新機能を以下のインストラクションでご確認いただけます。",
+      "NEWデザインをぜひご利用ください。よろしくお願いいたします。",
+    ],
+  },
+  // FEATURE 2
+  {
+    trans_id: "feature_2_header",
+    en: "More Markets",
+    zh: "更多盘口",
+    vi: "Đa Dạng Cược Hơn",
+    id: "Lebih Banyak Pasaran",
+    th: "ตลาดเดิมพันมากขึ้น",
+    ko: "다양한 베팅 마켓",
+    pt: "Mais Mercados",
+    km: "ទីផ្សារភ្នាល់ច្រើនទៀត",
+    ja: "その他マーケット",
+  },
+  {
+    trans_id: "feature_2_content",
+    en: [
+      "View more markets on the Events page.",
+      "View all Handicap and OU lines on the Events page by extending. Other markets like Corners and Bookings can also be viewed on the Events page, when available.",
+    ],
+    zh: [
+      "在赛事页面查看盘口",
+      "透过展开在赛事页面查看让球和大小盘，其他盘口如角球、罚球也可在此查看。",
+    ],
+    vi: [
+      "Xem thêm các loại cược mới tại Trang Thể Thao.",
+      "Xem tất cả các Cược Chấp và Cược Trên/Dưới trên Trang Thể thao bằng cách mở rộng trang. Các cược khác như Phạt Góc và Thẻ Phạt cũng có thể được hiển thị trên trang Thể Thao, khi sẵn có.",
+    ],
+    id: [
+      "Lihat lebih banyak pasaran pada halaman Pertandingan.",
+      "Lihat semua pasaran Handicap dan Atas/Bawah pada halaman Olahraga dengan di perluas. Pasaran lainnya seperti Tendangan Sudut dan Bookings juga bisa di lihat pada halaman Olahraga, saat tersedia.",
+    ],
+    th: [
+      "ดููตลาดเดิมพันมากขึ้นได้ที่หน้าอีเวนท์",
+      "ขยายเพื่อดูการเดิมพันแฮนดิแคปและสูงต่ำทั้งหมด โดยการเดิมพันตลาดอื่น ๆ เช่น เตะมุม ใบเหลือง/ใบแดง สามารถดูได้ในหน้าอีเวนท์เมื่อมีการเปิดให้เดิมพัน",
+    ],
+    ko: [
+      "이벤트 페이지에서 다양한 베팅 마켓을 확인하세요.",
+      "이벤트 페이지를 확장하여 모든 핸디캡과 OU 베팅을 확인하세요. 코너와 반칙등 다른 베팅 옵션도 곧 이벤트 페이지에서 확인하실 수 있습니다.",
+    ],
+    pt: [
+      "Ver mais mercados na página dos Eventos",
+      "Visualize todas as linhas de Handcap e OU estendendo a página de Eventos. Outros mercados como Escanteios e Apostas podem ser visualizadas na página de Eventos quando disponível.",
+    ],
+    km: [
+      "មើលទីផ្សារភ្នាល់ច្រើនទៀតនៅលើទំព័រកីឡា។",
+      "ពង្រីកដើម្បីមើលទីផ្សារគ្រាប់ត និងលើស/ក្រោមទាំងអស់នៅលើទំព័រកីឡា។ ទីផ្សារភ្នាល់ផ្សេងទៀតដូចជាបាល់ជ្រុង និងកាត ក៏ត្រូវបានបង្ហាញនៅលើទំព័រកីឡានេះនៅដែលវាមានផ្តល់។",
+    ],
+    ja: [
+      "イベントページでその他マーケットを確認",
+      "イベントページでは全てのハンディキャップとオーバー/アンダー欄を拡張して表示いただけます。その他、 コーナーやブッキングなどのマーケットが利用可能な場合、イベントページで表示できます。",
+    ],
+  },
+  // FEATURE 3
+  {
+    trans_id: "feature_3_header",
+    en: "Market to Market Swiping",
+    zh: "滑动查看盘口",
+    vi: "Vuốt Ngang Để Xem Cược",
+    id: "Menggeser Pasaran ke Pasaran",
+    th: "สไลด์เพื่อดูเดิมพันทั้งหมด",
+    ko: "스와이프를 통한 마켓 전환",
+    pt: "Mudando de Mercado para Mercado",
+    km: "អូសដើម្បីមើលទីផ្សារភ្នាល់",
+    ja: "各マーケット間をスワイプ移動",
+  },
+  {
+    trans_id: "feature_3_content",
+    en: [
+      "On smaller devices, swipe left and right to see more markets on the Events page.",
+      "On smaller screens, see more markets on our Events page by swiping left and right. We will continue to expand this through sports to allow you see a wider range of markets from the Events page.",
+    ],
+    zh: [
+      "在赛事页面，向左右滑动即可查看更多盘口。",
+      "在较小的屏幕上，通过左右滑动即可查看更多的赛事盘口，我们将陆续提供更多盘口。",
+    ],
+    vi: [
+      "Tối ưu trải nghiệm trên màn hình nhỏ - vuốt sang trái và phải để xem thêm cược.",
+      "Trải nghiệm mượt mà hơn tại màn hình nhỏ khi xem các loại cược cho mỗi Trận Đấu chỉ với thao tác vuốt sang trái hoặc phải. Đồng thời tối đa trận đấu trên 1 màn hình.",
+    ],
+    id: [
+      "Pada layar yang lebih kecil, geser kiri dan kanan untuk melihat lebih banyak pasaran pada halaman Olahraga.",
+      "Pada layar yang lebih kecil, lihat lebih banyak pasaran pada halaman Pertandingan kami dengan cara geser kiri dan kanan. Kami akan melanjutkan pengembangan ini melalui olahraga untuk memudahkan Anda melihat tampilan pasaran yang lebih luas dari halaman Pertandingan.",
+    ],
+    th: [
+      "มือถือหน้าจอขนาดเล็ก ให้สไลด์ซ้ายหรือขวา เพื่อดูตลาดเดิมพันทั้งหมด",
+      "มือถือหน้าจอขนาดเล็ก ให้สไลด์ซ้ายหรือขวา เพื่อดูตลาดเดิมพันทั้งหมด โดยคุณสามารถกดขยายเพื่อให้เห็นเดิมพันที่มากขึ้นในหน้าอีเวนท์",
+    ],
+    ko: [
+      "스와이프 기능을 통해 보다 작은 스크린에서도 쉽고 빠르게, 다양한 베팅 마켓을 확인하실 수 있습니다.",
+      "스와이프 기능을 통해 보다 작은 스크린에서도 쉽고 빠르게, 다양한 베팅 마켓을 확인하세요. 더 많은 마켓을 확인하실 수 있도록 계속해서 기능이 향상될 예정입니다.",
+    ],
+    pt: [
+      "Em pequenos dispositivos, mude para a esquerda e direita para ver mais mercados na página de eventos.",
+      "Em telas menores, veja mais mercados da nossas páginas de Eventos mudando para esquerda e direita. Nos continuaremos expandindo isso por meio de esportes para permitir que você veja uma gama mais ampla de mercados na página de Eventos.",
+    ],
+    km: [
+      "នៅលើឧបករណ៍តូចៗ អូសទៅឆ្វេង និងស្តាំដើម្បីមើលទីផ្សារភ្នាល់ច្រើនទៀតនៅលើទំព័រកីឡា។",
+      "នៅលើអេក្រង់តូចជាងមុន អាចមើលទីផ្សារភ្នាល់ច្រើនទៀតនៅលើទំព័រកីឡារបស់យើងដោយអូសទៅឆ្វេង​ និងស្តាំ។ យើងនឹងបន្តការអភិវឌ្ឍនេះតាមរយៈកីឡា ដើម្បីធ្វើឱ្យអ្នកមានភាពងាយស្រួលក្នុងការមើលឃើញទីផ្សារកាន់តែទូលំទូលាយពីទំព័រកីឡា។",
+    ],
+    ja: [
+      "小型デバイスでは、左右にスワイプしてイベントページで他のマーケットを表示いただけます。",
+      "画面上を左右にスワイプして、イベントページで多くのマーケットを表示いただけます。イベントページからより多くのマーケットをご覧いただけるように、今後も スポーツページの機能拡張をしていきます。",
+    ],
+  },
+  // FEATURE 4
+  {
+    trans_id: "feature_4_header",
+    en: "Price Boosts",
+    zh: "更高赔率",
+    vi: "Cược Đặc Biệt",
+    id: "Harga Spesial",
+    th: "ราคาพิเศษ",
+    ko: "고배당 스페셜",
+    pt: "Cotação Turbo",
+    km: "តម្លៃពិសេស",
+    ja: "プライス・ブースト",
+  },
+  {
+    trans_id: "feature_4_content",
+    en: [
+      "All selections with enhanced odds are now in one page.",
+      "On certain events, we offer enhanced price boosts on some selections. The new Price Boosts page allows you see all events with Price Boosts, across each sport.",
+    ],
+    zh: [
+      "所有的更高赔率盘口将显示在同一个页面。",
+      "对于部分赛事，我们提供更高赔率的盘口，您可以在该页面查看到不同的体育赛事。",
+    ],
+    vi: [
+      "Tất cả các cược với tỷ lệ cược cao hơn sẽ xuất hiện trên cùng một trang.",
+      "Trang Cược Đặc biệt mới cho phép Quý Khách theo dõi tất cả Cược Đặc biệt - Tỷ Lệ Cược Cao Hơn tại tất cả các trận cho mỗi môn thể thao.",
+    ],
+    id: [
+      "Semua pilihan dengan harga spesial sekarang ada di dalam satu halaman.",
+      "Pada pertandingan tertentu, kami menawarkan harga spesial pada beberapa pilihan. Halaman Harga Spesial yang baru memungkinkan Anda untuk melihat seluruh pertandingan dengan Harga Spesial, di setiap pertandingan.",
+    ],
+    th: [
+      "ตัวเลือกราคาพิเศษทั้งหมดอยู่ในหน้าเดียวกันแล้ว",
+      "ราคาพิเศษจะรวมทุกคู่แข่งขันที่มีราคาพิเศษ โดยนำมารวมไว้ในหน้าเดียว แยกเป็นแต่ละประเภทกีฬา เพื่อการดูที่สะดวกยิ่งขึ้น",
+    ],
+    ko: [
+      "모든 고배당 스페셜 마켓을 한 페이지에서 확인하실 수 있습니다.",
+      "주요 경기에 대한 고배당 스페셜 마켓 을 제공합니다. 각 스포츠 항목에 대한 고배당 스페셜 마켓을 새로운 고배당 스페셜 페이지에서 확인하실 수 있습니다.",
+    ],
+    pt: [
+      "Todas as seleções com cotações melhores estão agora em uma página.",
+      "Em certos eventos, nós oferecemos cotação turbo melhores em algumas seleções. A nova página de cotação turbo permite você visualizar todos os eventos com Cotação Turbo, em cada esporte.",
+    ],
+    km: [
+      "ឥឡូវនេះជំរើសទាំងអស់ ដែលមានហាងឆេងខ្ពស់មាននៅក្នុងទំព័រតែមួយ។",
+      "នៅក្នុងព្រឹត្តិការណ៍ជាក់លាក់ យើងផ្តល់ជូននូវតម្លៃពិសេសលើជម្រើសមួយចំនួន។ ទំព័រតម្លៃពិសេសថ្មី អនុញ្ញាតឱ្យអ្នកមើលឃើញព្រឹត្តិការណ៍ទាំងអស់ជាមួយនឹងតម្លៃពិសេស សម្រាប់កីឡានីមួយៗ។",
+    ],
+    ja: [
+      "特別強化オッズを利用できる全てのセレクションが1ページに集約されました。",
+      "一部のイベントでは、一部のセレクションでのみプライス・ブーストを提供しています。 新しいプライス・ブーストページでは、各スポーツのプライス・ブーストに関する全てのイベントをご確認いただけます。",
+    ],
+  },
+  // FEATURE 5
+  {
+    trans_id: "feature_5_header",
+    en: "Multiple Accumulator Price",
+    zh: "复式过关赔率",
+    vi: "Tỷ Lệ Cược Kết Hợp",
+    id: "Akumulator Odds Parlay",
+    th: "ราคาคูณของบิลสเต็ป",
+    ko: "폴드 누적 베팅 금액",
+    pt: "Cotação Múltipla Acumulada",
+    km: "តម្លៃបន្សំហាងឆេងគូវិល",
+    ja: "マルチプルアキュムレータ・プライス",
+  },
+  {
+    trans_id: "feature_5_content",
+    en: [
+      "While creating a Multiple Bet, you can see the accumulator Multiple Bet price in the Bet Slip button.",
+      "This price will update as you add / remove selections in your Bet Slip.",
+    ],
+    zh: [
+      "在投注复式过关时，您可以在投注单按钮查看复式过关的赔率。",
+      "当您添加/删除注单时，赔率会更新。",
+    ],
+    vi: [
+      "Khi đặt Cược Xâu, Quý Khách có thể xem hệ số Tỉ Lệ Cược Kết Hợp tại Vé Cược.",
+      "Tỉ lệ này sẽ thay đổi khi thêm hoặc bớt các lựa chọn trong xâu.",
+    ],
+    id: [
+      "Saat membuat Taruhan Parlay, Anda dapat melihat akumulator Odds Taruhan Parlay di tombol Slip Taruhan.",
+      "Odds ini akan diperbarui saat Anda menambah / menghapus pilihan dalam Slip Taruhan Anda.",
+    ],
+    th: [
+      "คุณสามารถดูราคาคูณทั้งหมดของบิลสเต็ปได้ในปุ่มสลิปเดิมพัน",
+      "ราคาคูณจะอัปเดตทันทีเมื่อคุณเพิ่มหรือลบคู่แข่งขันในสลิปเดิมพันของคุณ",
+    ],
+    ko: [
+      "조합 베팅시, 폴드 누적 베팅 금액을 베팅 슬립 버튼에서 확인하실 수 있습니다.",
+      "이 베팅 금액은 항목 변경시 베팅 슬립에 실시간 업데이트 됩니다.",
+    ],
+    pt: [
+      "Enquanto você estiver criando sua aposta Múltipla, você pode ver a cotação da Aposta Múltipla Acumulada no botão Boletin de Apostas.",
+      "Esta cotação será atualizada quando você adicionar / remover as seleções no seu Boletin de Aposta.",
+    ],
+    km: [
+      "នៅពេលធ្វើការភ្នាល់គូវិល អ្នកអាចមើលឃើញតម្លៃបន្សំហាងឆេងគូវិលនៅក្នុងប៊ូតុងប័ណ្ណភ្នាល់។",
+      "បន្សំហាងឆេងនេះ នឹងត្រូវបានធ្វើការកែប្រែនៅពេលដែលអ្នកបន្ថែម/ដកចេញនូវជម្រើសនៅក្នុងប័ណ្ណភ្នាល់របស់អ្នក។",
+    ],
+    ja: [
+      "マルチプルベットの作成時に、ベットスリップボタンでアキュムレータのマルチプルベット・プライスをご確認いただけます。",
+      "このプライスは、ベットスリップでセレクションを追加または削除すると更新されます。",
+    ],
+  },
+  // FEATURE 6
+  {
+    trans_id: "feature_6_header",
+    en: "Remember My Stake",
+    zh: "常用投注额",
+    vi: "Lưu Tiền Cược",
+    id: "Ingat Jumlah Taruhan Saya",
+    th: "จำยอดเงินเดิมพัน",
+    ko: "베팅 금액 저장",
+    pt: "Lembrar Meu Valor Apostado",
+    km: "ចងចាំប្រាក់ភ្នាល់របស់ខ្ញុំ",
+    ja: "マイベットを記憶",
+  },
+  {
+    trans_id: "feature_6_content",
+    en: [
+      "If you usually bet the same stake, you can select the ‘Remember Stake’ option when entering your stake.",
+      "Your last stake will then be remembered the next time you want to place a bet.",
+    ],
+    zh: [
+      '经常使用相同的金额下注，您可在输入投注金额后，选择"常用投注额"选项。',
+      "您最后选择的常用投注额会在下次投注时自动显示。",
+    ],
+    vi: [
+      "Tính năng “Lưu Tiền Cược” sẽ giúp hiển thị nhanh chóng Tiền Cược gần nhất được lưu nếu Quý Khách thường đặt cược cùng mức tiền.",
+      "Tiền Cược gần nhất của Quý Khách  sẽ được lưu và tự động hiển thị vào lần đặt cược tiếp theo.",
+    ],
+    id: [
+      "Jika Anda biasanya bertaruh pada jumlah taruhan yang sama, Anda dapat memilih 'Ingat Jumlah Taruhan' saat memasukkan taruhan Anda.",
+      "Jumlah taruhan terakhir Anda akan di simpan sampai Anda ingin memasang taruhan berikutnya.",
+    ],
+    th: [
+      'หากคุณชอบเดิมพันด้วยยอดเงินเดียวกันบ่อย ๆ คุณสามารถเปิดใช้งาน "จำยอดเงินเดิมพัน" ได้',
+      "เมื่อเปิดใช้งาน เงินเดิมพันล่าสุดของคุณจะถูกจำไว้แสดงในครั้งต่อไปที่คุณวางเดิมพัน",
+    ],
+    ko: [
+      '베팅시, 선호하시는 베팅 금액을 "베팅 금액 저장" 기능을 통해 등록하실 수 있습니다.',
+      "마지막 베팅 금액이 저장되어 다음 베팅시 자동으로 선택됩니다.",
+    ],
+    pt: [
+      'Se você normalmente aposta o mesmo valor, você poderá selecionar a opção "Lembrar Valor Apostado" quando colocar o valor.',
+      "Seu último valor apostado será gravado para a próxima vez que você quiser colocar uma aposta.",
+    ],
+    km: [
+      "ប្រសិនបើជាធម្មតា អ្នកភ្នាល់ចំនួនប្រាក់ភ្នាល់ដូចគ្នា អ្នកអាចជ្រើសជំរើស “ប្រាក់ភ្នាល់ត្រូវចងចាំ” នៅពេលបញ្ចូលប្រាក់ភ្នាល់របស់អ្នក។",
+      "ប្រាក់ភ្នាល់ចុងក្រោយរបស់អ្នក នឹងត្រូវបានចងចាំនៅដែលអ្នកចង់ភ្នាល់ពេលក្រោយ។",
+    ],
+    ja: [
+      "いつもと同じ金額をベットする場合、‘ベット額を記憶’オプションから同額ベットをご選択いただけます。",
+      "プレイヤーの最後のベット額が、次のベット時にてご利用いただけるように記憶します。",
+    ],
+  },
+  {
+    trans_id: "learn-more-link",
+    en: "Learn More",
+    zh: "查看更多",
+    vi: "Tìm hiểu thêm",
+    id: "Pelajari Lebih Banyak",
+    th: "เรียนรู้เพิ่มเติม",
+    ko: "더 알아보기",
+    pt: "Saiba Mais",
+    km: "ស្វែង​យល់បន្ថែម",
+    ja: "詳細確認",
+  },
+];
+
+// Language Code of the HTML
+const { lang } = document.documentElement;
+
+// Generating the translated data inside the HTML
+translationData.forEach((objData) => {
+  const $objectEl = document.getElementById(objData.trans_id);
+
+  if ($objectEl) {
+    if (typeof objData[lang] === "object") {
+      // FOR PARAGRAPHS
+      objData[lang].forEach((textData) => {
+        const $newParaEl = document.createElement("p");
+        $newParaEl.innerHTML = textData;
+        $objectEl.appendChild($newParaEl);
+      });
+    } else if (typeof objData[lang] === "string") {
+      // FOR SINGLE-LINE TEXTS
+      $objectEl.innerHTML = objData[lang];
+    }
+  }
+});
